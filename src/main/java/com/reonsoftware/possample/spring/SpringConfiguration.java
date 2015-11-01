3bcb4004c35d18f490e27c7c0de21de96464dd1d
Jon Onstott <jononstott@gmail.com>
2015-10-31 18:26:19 -0700

    Created the IntelliJ project.  Will run in Tomcat using Java 8.

---
+++ src/main/java/com/reonsoftware/possample/spring/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package com.reonsoftware.possample.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jon Onstott
+ * @since 10/31/2015
+ */
+@SpringBootApplication
+public class SpringConfiguration {
+}
