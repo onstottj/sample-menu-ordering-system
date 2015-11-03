46e619b0e78b193ab3e298b041fa3cab8be4eb39
Jon Onstott <jononstott@gmail.com>
2015-11-03 12:21:18 -0800

    Added README.md and the DB SQL

--- README.md
+++ README.md
@@ -14,3 +14,23 @@
     `---'.|        `---`                                   |  ,     .-./'---'           `---'.|    ---`-'   \   \  /
       `---`                                                 `--`---'                      `---`              `----'
 
+
+Overview
+--------
+
+This is a sample AngularJS restaurant point-of-sale web app, with a REST backend written in Spring.  It uses Spring
+Boot, so to run it, the main class is `com.reonsoftware.possample.Application`.
+
+Installation
+------------
+
+ - The database used is MySQL, and the DDL is in database.sql in this directory
+ - The database name is 'pos', with a user 'pos' with password 'pos'.  These details are hard-coded in
+   com.reonsoftware.possample.Application for now.
+
+Caveats
+-------
+
+ - Flexbox was used for UI layout, so please view the site using a modern browser
+ - NPM was used to pull in dependencies like Angular.  Typically I'd exclude the node_modules folder from source
+   control, but in this case I'm including it so that you don't have to install Node on your system.
---
+++ database.sql
@@ -0,0 +1,53 @@
+CREATE TABLE `items` (
+	`item_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name`    VARCHAR(45)      DEFAULT NULL,
+	`price`   DECIMAL(15, 2)   DEFAULT NULL,
+	PRIMARY KEY (`item_id`),
+	UNIQUE KEY `item_name_uk` (`name`)
+)
+	ENGINE = InnoDB
+	AUTO_INCREMENT = 123
+	DEFAULT CHARSET = utf8;
+
+CREATE TABLE `order_line_items` (
+	`order_line_item_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`order_id`           INT(11) NOT NULL,
+	`item_id`            INT(11) NOT NULL,
+	`quantity`           INT(11) NOT NULL,
+	PRIMARY KEY (`order_line_item_id`),
+	KEY `oli_items_fk_idx` (`item_id`),
+	KEY `oli_orders_fk_idx` (`order_id`),
+	CONSTRAINT `oli_items_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION,
+	CONSTRAINT `oli_orders_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+	ENGINE = InnoDB
+	AUTO_INCREMENT = 5
+	DEFAULT CHARSET = utf8;
+
+CREATE TABLE `orders` (
+	`order_id`     INT(11) NOT NULL AUTO_INCREMENT,
+	`order_number` INT(11)          DEFAULT NULL,
+	PRIMARY KEY (`order_id`),
+	UNIQUE KEY `order_number_uk` (`order_number`)
+)
+	ENGINE = InnoDB
+	AUTO_INCREMENT = 12
+	DEFAULT CHARSET = utf8;
+
+CREATE TABLE `tender` (
+	`tender_id`       INT(11)        NOT NULL AUTO_INCREMENT,
+	`order_id`        INT(11)        NOT NULL,
+	`amount_tendered` DECIMAL(15, 2) NOT NULL,
+	`change_due`      DECIMAL(15, 2) NOT NULL,
+	PRIMARY KEY (`tender_id`),
+	KEY `tender_order_fk_idx` (`order_id`),
+	CONSTRAINT `tender_order_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+	ENGINE = InnoDB
+	DEFAULT CHARSET = utf8;
