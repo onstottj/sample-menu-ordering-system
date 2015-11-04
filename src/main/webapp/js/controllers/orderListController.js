bf392e81c0669caf3eeaa55a029f19ea55f73944
Jon Onstott <jononstott@gmail.com>
2015-11-03 19:00:02 -0800

    Set up order list toggle buttons

--- src/main/webapp/js/controllers/orderListController.js
+++ src/main/webapp/js/controllers/orderListController.js
@@ -5,4 +5,24 @@
 posModule.controller('OrderListController', function ($scope, viewManager) {
     // Proxy some orderStatus values/functions.  Maybe there's a better way to do this (like wrapping these in an object)?
     $scope.isShowingList = viewManager.isShowingList;
+
+    $scope.allOrders = [];
+
+    $scope.ordersExist = function () {
+        return $scope.allOrders.length > 0;
+    };
+
+    $scope.showInProgressOrders = false;
+    $scope.showUnpaidOrders = false;
+    $scope.showPaidOrders = false;
+
+    $scope.toggleShowAll = function () {
+        var newState = !$scope.areAllShown();
+        $scope.showInProgressOrders = newState;
+        $scope.showUnpaidOrders = newState;
+        $scope.showPaidOrders = newState;
+    };
+    $scope.areAllShown = function () {
+        return $scope.showInProgressOrders && $scope.showUnpaidOrders && $scope.showPaidOrders;
+    };
 });
